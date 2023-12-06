929492f78d2254ce58b08efcf13c0dc9c5014ef2
Pavel <podoprigoraps@gmail.com>
2023-12-06 23:09:29 +0400

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleBurgerMenu() {
+    var burgerMenu = document.querySelector('.burger-menu');
+    burgerMenu.classList.toggle('open');
+}
+
+function closeBurgerMenu() {
+    var burgerMenu = document.querySelector('.burger-menu');
+    burgerMenu.classList.remove('open');
+}
